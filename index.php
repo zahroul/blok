1cdea078ebe895c76721177e124b1ad64061384e
Zahroul Ulum <moh.zahroul@gmail.com>
2017-10-30 09:45:10 +0700

    Add different site-title selector It won't be displayed in the site's front and home pages.

--- index.php
+++ index.php
@@ -18,9 +18,15 @@
 
 	<body>
 		<header>
-			<h1 class="site-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-			</h1>
+			<?php if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+				</h1>
+			<?php else : ?>
+				<p class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+				</p>
+			<?php endif; ?>
 		</header>
 
 		<main>
